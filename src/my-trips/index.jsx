3754d451d763f1e4b6f6982ccd1db10192f9c9e5
Jahnavi3045 <jahnavi3045@gmail.com>
2025-04-02 16:50:51 +0530

    first commit

--- src/my-trips/index.jsx
+++ src/my-trips/index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useNavigation } from 'react-router-dom';
 import { db } from '@/service/firebaseConfig'
 import { collection, getDocs, query, where } from 'firebase/firestore'
-import UserTripCardItem from './components/userTripCardItem';
+import UserTripCardItem from './components/UserTripCardItem';
 function MyTrips() {
     const navigation = useNavigation();
     const [userTrips, setUserTrips] = useState([]);
